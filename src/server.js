d7d7efc09f10cd4a8f000a707692801029efa3cb
Lucas Lessa <lucaslessa14@hotmail.com>
2020-10-10 19:35:50 -0300

    viewport configurado

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 require('dotenv/config');
 
+const port = process.env.PORT || 8008;
 const express = require('express');
 const path = require('path');
 const app = express();
@@ -9,13 +10,10 @@ const routes = require('./routes.js');
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+app.use(express.static(__dirname + '/public'));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.use(express.static(__dirname + '/public'));
-
 app.use('/', routes);
 
-app.listen(process.env.PORT || 8008, () => {
-    console.log("Server rodando na porta " + process.env.PORT);
-})
+app.listen(port, () => console.log(`Server rodando na porta ${ port }`));
